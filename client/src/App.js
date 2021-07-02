bebdb3c0269e48f77e82ffbd8820b8088f67f9e6
Kallyas <kallyasmedia@gmail.com>
2021-07-02 18:52:35 +0300

    add new assets

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,10 @@ import useToken from "./Utils/useToken";
 import Home from "./components/Home/Home";
 import Landing from "./components/Landing/Landing";
 import ProtectedRoute from "./components/Common/ProtectedRoute";
+import ProtectedRouteWithSidebar from "./components/Common/ProtectedRouteWithSidebar"
 import PublicRoute from "./components/Common/PublicRoute";
+
+import UserDashboard from "./pages/dashboard/UserDashboard"
 import Signin from "./pages/Signin";
 import Signup from "./pages/signup"
 import NotFound from "./pages/NotFound"
@@ -23,7 +26,7 @@ function App() {
         <Route exact path={Routes.LandingPage.path} >
           { token ? <Redirect to={Routes.UserDashboard.path} /> : <Landing />}
           </Route>
-        <ProtectedRoute exact path={Routes.UserDashboard.path} component={Home} />
+        <ProtectedRouteWithSidebar exact path={Routes.UserDashboard.path} component={UserDashboard} />
         <PublicRoute exact path={Routes.SignIn.path} component={Signin} />
         <PublicRoute exact path={Routes.SignUp.path} component={Signup} />
         <ProtectedRoute path="/add" component={Home}/>
