e61b69101b174167deb2f27c53a8cf9650d56b56
Kallyas <kallyasmedia@gmail.com>
2021-07-02 16:30:29 +0300

    add: sign in page

---
+++ client/src/pages/Signin.js
@@ -0,0 +1,72 @@
+
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faUnlockAlt } from "@fortawesome/free-solid-svg-icons";
+import { Col, Row, Form, Card, Button, FormCheck, Container, InputGroup } from '@themesberg/react-bootstrap';
+import { Link } from 'react-router-dom';
+
+// import { Routes } from "../../routes";
+
+
+
+const  Signin = () => {
+  return (
+    <main>
+      <section className="d-flex align-items-center my-5 mt-lg-6 mb-lg-5">
+        <Container>
+          <Row className="justify-content-center form-bg-image">
+            <Col xs={12} className="d-flex align-items-center justify-content-center">
+              <div className="bg-white shadow-soft border rounded border-light p-4 p-lg-5 w-100 fmxw-500">
+                <div className="text-center text-md-center mb-4 mt-md-0">
+                  <h3 className="mb-0">Sign into your account</h3>
+                </div>
+                <Form className="mt-4">
+                  <Form.Group id="email" className="mb-4">
+                    <Form.Label>Your Email</Form.Label>
+                    <InputGroup>
+                      <InputGroup.Text>
+                        <FontAwesomeIcon icon={faEnvelope} />
+                      </InputGroup.Text>
+                      <Form.Control autoFocus required type="email" placeholder="example@company.com" />
+                    </InputGroup>
+                  </Form.Group>
+                  <Form.Group>
+                    <Form.Group id="password" className="mb-4">
+                      <Form.Label>Your Password</Form.Label>
+                      <InputGroup>
+                        <InputGroup.Text>
+                          <FontAwesomeIcon icon={faUnlockAlt} />
+                        </InputGroup.Text>
+                        <Form.Control required type="password" placeholder="Password" />
+                      </InputGroup>
+                    </Form.Group>
+                    <div className="d-flex justify-content-between align-items-center mb-4">
+                      <Form.Check type="checkbox">
+                        <FormCheck.Input id="defaultCheck5" className="me-2" />
+                        <FormCheck.Label htmlFor="defaultCheck5" className="mb-0">Remember me</FormCheck.Label>
+                      </Form.Check>
+                      <Card.Link className="small text-end">Lost password?</Card.Link>
+                    </div>
+                  </Form.Group>
+                  <Button variant="primary" type="submit" className="w-100">
+                    Sign in
+                  </Button>
+                </Form>
+                <div className="d-flex justify-content-center align-items-center mt-4">
+                  <span className="fw-normal">
+                    Not registered?
+                    <Card.Link as={Link} to={"/"} className="fw-bold">
+                      {` Create account `}
+                    </Card.Link>
+                  </span>
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </main>
+  );
+};
+
+export default Signin
