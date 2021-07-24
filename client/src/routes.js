03ce233fbba985eae5a33abba3492ed35f1fe8d9
Kallyas <kallyasmedia@gmail.com>
2021-07-24 17:42:39 +0300

    Add new routes

--- client/src/routes.js
+++ client/src/routes.js
@@ -5,5 +5,5 @@ export const Routes = {
     SignIn: { path: "/auth/login"},
     SignUp: { path: "/auth/signup"},
     Transactions: { path: "/user/dashboard/transactions" },
-    Details: { path: "/user/dashboard/details/:id" }
+    Details: { path: "/user/dashboard/details" }
 }
