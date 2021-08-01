953feb58284df99ec71a2d644feb6235ec5b148e
Kallyas <kallyasmedia@gmail.com>
2021-08-01 18:56:26 +0300

    add pages index

---
+++ client/src/pages/index.js
@@ -0,0 +1,17 @@
+import ParcelDetails from './ParcelDetails'
+import NotFound from './NotFound'
+import Signin from './Signin'
+import Signup from './signup'
+import Transactions from './Transactions'
+import UserDashboard from './dashboard/UserDashboard'
+import AdminDashboard from './dashboard/AdminDashboard'
+
+export {
+    ParcelDetails,
+    NotFound,
+    Signin,
+    Signup,
+    Transactions,
+    UserDashboard,
+    AdminDashboard
+}
