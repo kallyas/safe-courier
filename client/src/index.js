83e65465e2f7b4b86b01d02d62c405b4d5254802
kallyas <kallyasmedia@gmail.com>
2022-01-27 16:17:21 +0000

    init v3

--- client/src/index.js
+++ client/src/index.js
@@ -1,22 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter } from "react-router-dom";
-
-// core styles
-import "./scss/volt.scss";
-import "@fortawesome/fontawesome-free/css/all.css";
-import "react-datetime/css/react-datetime.css";
-
-
+import './index.css';
 import App from './App';
-import { ScrollToTop } from "./components/index"
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter basename="/">
-      <ScrollToTop />
-      <App />
-    </HashRouter>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
