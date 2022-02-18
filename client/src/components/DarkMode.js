2bf34fc0330f2c5ae7583db9e6bad7fecfbc23fb
kallyas <kallyasmedia@gmail.com>
2022-02-18 19:45:04 +0300

    ch: add dark mode support

---
+++ client/src/components/DarkMode.js
@@ -0,0 +1,24 @@
+import { ActionIcon, useMantineColorScheme } from "@mantine/core";
+import { SunIcon, MoonIcon } from "@modulz/radix-icons";
+
+export const DarkMode = () => {
+  const { colorScheme, toggleColorScheme } = useMantineColorScheme();
+  const dark = colorScheme === "dark";
+
+  return (
+    <ActionIcon
+      variant="outline"
+      color={dark ? "yellow" : "blue"}
+      onClick={() => toggleColorScheme()}
+      title="Toggle color scheme"
+    >
+      {dark ? (
+        <SunIcon style={{ width: 18, height: 18 }} />
+      ) : (
+        <MoonIcon style={{ width: 18, height: 18 }} />
+      )}
+    </ActionIcon>
+  );
+};
+
+export default DarkMode;
