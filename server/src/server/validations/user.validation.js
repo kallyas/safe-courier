9518cdd10f507d3b506b4e3c44579e18dc51bddf
kallyas <kallyasmedia@gmail.com>
2022-07-23 01:13:06 +0300

    ch: add validations

---
+++ server/src/server/validations/user.validation.js
@@ -0,0 +1,46 @@
+import Joi from "joi";
+import { password, objectId } from "./custom.validation.js";
+
+
+const getUsers = {
+  query: Joi.object().keys({
+    name: Joi.string(),
+    role: Joi.string(),
+    sortBy: Joi.string(),
+    limit: Joi.number().integer(),
+    page: Joi.number().integer(),
+  }),
+};
+
+const getUser = {
+  params: Joi.object().keys({
+    userId: Joi.string().custom(objectId),
+  }),
+};
+
+const updateUser = {
+  params: Joi.object().keys({
+    userId: Joi.required().custom(objectId),
+  }),
+  body: Joi.object()
+    .keys({
+      name: Joi.string(),
+      email: Joi.string()
+    })
+    .min(1),
+};
+
+const deleteUser = {
+  params: Joi.object().keys({
+    userId: Joi.string().custom(objectId),
+  }),
+};
+
+const userValidation = {
+  getUsers,
+  getUser,
+  updateUser,
+  deleteUser,
+};
+
+export default userValidation;
