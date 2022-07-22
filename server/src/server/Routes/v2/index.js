4d53dffdf4e2f251275e776aad498dd6ef504034
kallyas <kallyasmedia@gmail.com>
2022-07-23 01:14:31 +0300

    ch: create v2 routes

---
+++ server/src/server/Routes/v2/index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import authRoute from "./auth.route.js";
+import userRoute from "./user.route.js";
+
+const router = express.Router();
+
+const defaultRoutes = [
+  {
+    path: "/auth",
+    route: authRoute,
+  },
+];
+
+const userRoutes = [
+  {
+    path: "/users",
+    route: userRoute,
+  },
+];
+
+defaultRoutes.forEach((route) => {
+    router.use(route.path, route.route);
+  });
+  
+userRoutes.forEach((route) => {
+    router.use(route.path, route.route);
+});
+  
+
+export default router;
